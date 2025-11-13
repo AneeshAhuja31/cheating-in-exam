521d42128f27a8734d3e3ff533a0c750a7f7f924
Aneesh Ahuja <aneeshahuja31@gmail.com>
2025-11-14 01:58:26 +0530

    Fix PORT environment variable parsing in main execution block

--- api.py
+++ api.py
@@ -121,5 +121,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    PORT = int(os.getenv("PORT"),8080)
+    PORT = int(os.getenv("PORT", "8080"))
     uvicorn.run(app, host="0.0.0.0", port=PORT)
